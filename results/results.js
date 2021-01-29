cad3181e2e5b8782fe8ff55e5a9155382b7a897a
patrick hrabos <phrabos@gmail.com>
2021-01-29 13:25:49 -0500

    add name to personalize results page

--- results/results.js
+++ results/results.js
@@ -10,7 +10,7 @@ resetButton.addEventListener('click', () =>{
 displayStats();
 const user = getUserStorage();
 if (user.greed < 6){
-    div.textContent = `Congrats you have accumulated ${user.money} coins while minimizing greed points to ${user.greed}`;
+    div.textContent = `Congrats ${user.name} you have accumulated ${user.money} coins while minimizing greed points to ${user.greed}`;
 } else if (user.greed < 76 && user.greed > 5 && user.money > 0){
-    div.textContent = `You made a some greedy decisions and some of them paid off earning you ${user.money} coins while amassing ${user.greed} greed points!`;
-} else div.textContent = `Your greed points of ${user.greed} has you on the path towards bankruptcy`;
+    div.textContent = `${user.name}, you made a some greedy decisions and some of them paid off earning you ${user.money} coins while amassing ${user.greed} greed points!`;
+} else div.textContent = `Your greed points of ${user.greed}, ${user.name}, has you on the path towards bankruptcy`;
