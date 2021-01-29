f585151425bb0e359d60f077ef65f393d4147cdf
patrick hrabos <phrabos@gmail.com>
2021-01-29 12:37:17 -0500

    fix results logic

--- results/results.js
+++ results/results.js
@@ -10,6 +10,6 @@ resetButton.addEventListener('click', () =>{
 const user = getUserStorage();
 if (user.greed < 6){
     div.textContent = `Congrats you have accumulated ${user.money} coins while minimizing greed points to ${user.greed}`;
-} else if (user.greed < 76 && user.greed > 5){
+} else if (user.greed < 76 && user.greed > 5 && user.money > 0){
     div.textContent = `You made a some greedy decisions and some of them paid off earning you ${user.money} coins while amassing ${user.greed} greed points!`;
 } else div.textContent = `Your greed points of ${user.greed} has you on the path towards bankruptcy`;
